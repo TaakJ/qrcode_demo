5ccdcb85ef0ece6c9aa890adcf43532a7a1bb628
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-04-04 15:11:45 +0700

    for deploy

--- qrcode_demo/settings.py
+++ qrcode_demo/settings.py
@@ -25,8 +25,7 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-wj(iy$6b+tu81v0t!lk^(6w_@yojb8tf!=v_uwbky0ge8xk*l='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
