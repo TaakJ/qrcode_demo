146e77672e35c2d8c2fa31f0ffe061a565d91f6c
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-04-04 14:03:45 +0700

    for test

--- qrcode_demo/settings.py
+++ qrcode_demo/settings.py
@@ -129,11 +129,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/1.9/howto/static-files/
 # STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
@@ -145,18 +140,18 @@ USE_TZ = True
 #     os.path.join(CORE_DIR, 'apps/static'),
 # )
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(CORE_DIR, 'apps/static/media')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR , 'apps/static')    
+STATIC_ROOT = os.path.join(CORE_DIR , 'apps/static')    
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR , 'apps/static/media')
+MEDIA_ROOT = os.path.join(CORE_DIR , 'apps/static/media')
 STATICFILES_DIRS = [
-    #os.path.join(BASE_DIR, "static_cdn"),                
+    # os.path.join(CORE_DIR, 'apps/static'),              
 ] 
 
 
