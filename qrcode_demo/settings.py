46eeb5dd8efec2bd0d3c578ea2f3b1959ad78896
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-04-04 13:46:36 +0700

    for deploy

--- qrcode_demo/settings.py
+++ qrcode_demo/settings.py
@@ -134,23 +134,31 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, 'apps/static'),
-)
-
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(CORE_DIR, 'apps/static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(CORE_DIR, 'apps/static/media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'apps/static')    
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(CORE_DIR, 'apps/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR , 'apps/static/media')
+STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "static_cdn"),                
+] 
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
