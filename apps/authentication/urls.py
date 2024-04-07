7d7d40e19fdec1866defbb65e685db50072e4be3
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-04-07 13:04:25 +0700

    for test

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -3,12 +3,13 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from django.urls import path
-from .views import login_view, register_user
+from django.urls import re_path
+from .views import login_view, register_user, logout_view
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('login/', login_view, name="login"),
-    path('register/', register_user, name="register"),
-    path("logout/", LogoutView.as_view(), name="logout")
+    re_path('login/', login_view, name="login"),
+    re_path('register/', register_user, name="register"),
+    re_path('logout/', logout_view, name="logout"),
+    # re_path("logout/", LogoutView.as_view(), name="logout_view")
 ]
